cb90b612754e3b0beca8d2125a6fbbd3dda96713
조용제 <whehdrms6810@naver.com>
2023-03-13 14:51:00 +0900

    [CREATE] solely.dart

---
+++ lib/model/solely.dart
@@ -0,0 +1,16 @@
+class Solely {
+  bool isWin;
+  bool isMvp = false;
+  bool isAce = false;
+  int kill;
+  int death;
+  int assist;
+  String loon;
+  String time;
+  String gameType;
+  int killInclude;
+  String firstSpell;
+  String secondSpell;
+  Solely(this.isWin,this.kill, this.death, this.assist, this.loon, this.time,
+      this.firstSpell, this.secondSpell, this.gameType, this.killInclude);
+}

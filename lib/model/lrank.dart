664cda127b44d7192361391a537145a44c2f4155
조용제 <whehdrms6810@naver.com>
2023-03-27 18:48:05 +0900

    [CREATE] lrank.dart

---
+++ lib/model/lrank.dart
@@ -0,0 +1,12 @@
+class LolRank {
+  String ranking;
+  int rank;
+  String profile;
+  String userName;
+  String name;
+  int grade;
+  int classes;
+
+  LolRank(this.ranking, this.rank, this.profile, this.userName, this.name,
+      this.grade, this.classes);
+}

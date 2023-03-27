16bcd5e1f043814aba33e699b49cb881b7e427ab
조용제 <whehdrms6810@naver.com>
2023-03-27 18:46:24 +0900

    [ADD] animation

--- lib/screens/bottombar/combination.dart
+++ lib/screens/bottombar/combination.dart
@@ -1,21 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Combination extends StatefulWidget {
-  const Combination({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<Combination> createState() => _CombinationState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _CombinationState extends State<Combination> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Color?> _animation;
+
+  bool _isPressed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = ColorTween(
+      begin: Colors.white,
+      end: Colors.black,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _onButtonPressed() {
+    if (!_isPressed) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+
+    _isPressed = !_isPressed;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
+      backgroundColor: _animation.value,
       body: Center(
-        child: Text("조합 페이지", style: TextStyle(fontSize: 30.sp, color: Colors.black),),
-      )
+        child: ElevatedButton(
+          onPressed: _onButtonPressed,
+          child: const Text('Change Color'),
+        ),
+      ),
     );
   }
 }

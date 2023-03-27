7f79907a4c6330671dc0d13e40145bf5aebbcd03
조용제 <whehdrms6810@naver.com>
2023-03-27 18:48:16 +0900

    [CREATE] vrank.dart

---
+++ lib/model/vrank.dart
@@ -0,0 +1,12 @@
+class ValRank {
+  String ranking;
+  int rank;
+  String profile;
+  String userName;
+  String name;
+  int grade;
+  int classes;
+
+  ValRank(this.ranking, this.rank, this.profile, this.userName, this.name,
+      this.grade, this.classes);
+}

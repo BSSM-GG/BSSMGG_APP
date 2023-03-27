7c2c4315955dbba3093b53d86166515f102b1162
조용제 <whehdrms6810@naver.com>
2023-03-27 18:48:39 +0900

    [CREATE] rank.dart

---
+++ lib/screens/bottombar/rank.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Rank extends StatefulWidget {
+  const Rank({super.key});
+
+  @override
+  State<Rank> createState() => _RankState();
+}
+
+class _RankState extends State<Rank> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

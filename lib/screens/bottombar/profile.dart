3f447bdf1ae3e3a638b83b5530db165eb83adef3
조용제 <whehdrms6810@naver.com>
2023-03-27 18:46:46 +0900

    [ADD] background color animation

--- lib/screens/bottombar/profile.dart
+++ lib/screens/bottombar/profile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:plz_set_ward_app/common/common.dart';
+import 'package:plz_set_ward_app/provider/emphaisis.dart';
+import 'package:provider/provider.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -11,11 +14,21 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    var emphasis = Provider.of<Emphaisis>(context);
     return Scaffold(
-      appBar: null,
-      body: Center(
-        child: Text("프로필 페이지", style: TextStyle(fontSize: 30.sp, color: Colors.black),),
-      )
-    );
+        appBar: null,
+        body: SafeArea(
+            child: AnimatedContainer(
+          duration: const Duration(milliseconds: 350),
+          color:
+              emphasis.ispressed ? Colors.black.withOpacity(0.9) : Colors.white,
+          width: double.infinity,
+          height: double.infinity.h,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [Text("프로필")],
+          ),
+        )));
   }
 }

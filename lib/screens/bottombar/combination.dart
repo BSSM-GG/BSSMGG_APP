d4f5e8e27696bf7bd07e9b72ea973d51cf07acb9
Lovingcats <whehdrms6810@naver.com>
2023-04-26 17:10:22 +0900

    [ADD] 내전 팀빌더 text

--- lib/screens/bottombar/combination.dart
+++ lib/screens/bottombar/combination.dart
@@ -1,63 +1,26 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<Color?> _animation;
-
-  bool _isPressed = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    );
-
-    _animation = ColorTween(
-      begin: Colors.white,
-      end: Colors.black,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
-
-    _animationController.addListener(() {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  void _onButtonPressed() {
-    if (!_isPressed) {
-      _animationController.forward();
-    } else {
-      _animationController.reverse();
-    }
-
-    _isPressed = !_isPressed;
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: _animation.value,
+      appBar: null,
       body: Center(
-        child: ElevatedButton(
-          onPressed: _onButtonPressed,
-          child: const Text('Change Color'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const[
+            Text(
+              "내전팀빌더",
+              style: TextStyle(fontSize: 50, color: Colors.red),
+            )
+          ],
         ),
       ),
     );
